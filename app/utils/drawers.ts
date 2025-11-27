fc28d7b1bcea99b0bc7e261242dbf8edf129e56c
fpgarciamtnz <fpgarcia@uoc.edu>
2025-11-27 23:23:53 +0100

    Remove 'Burrata' drawer from DRAWERS
    Removed the 'Burrata' drawer from the DRAWERS array.

--- app/utils/drawers.ts
+++ app/utils/drawers.ts
@@ -1,5 +1,5 @@
 export const DRAWERS = [
-  { name: 'Foccacia', icon: 'i-heroicons-cube', items: ['Foccacia Bread', 'Whipped Butter', 'Lovange'] },
+{ name: 'Foccacia', icon: 'i-heroicons-cube', items: ['Foccacia Bread', 'Whipped Butter', 'Lovange'] },
 { name: 'Olive', icon: 'i-heroicons-sparkles', items: [] },
 { name: 'Canned Fish', icon: 'i-heroicons-cake', items: ['Ali Oli', 'Cut Lemons', 'Vaccum Jalapeños'] },
 { name: 'Tuna Tartar', icon: 'i-heroicons-gift', items: ['Cut Tuna', 'Cut Tomatoes Cherry', 'Tuna Dressing'] },
@@ -7,7 +7,6 @@ export const DRAWERS = [
 { name: 'Grilled Leeks', icon: 'i-heroicons-star', items: ['Cut leeks', 'Grill Leeks', 'Vinnagrete', 'Boil Eggs', 'Cut Anchovies', 'Croutons', 'Cut Tarragon', 'Prepare Parsley'] },
 { name: 'Pasta Bolognese', icon: 'i-heroicons-sun', items: ['Bolognese Sauce', 'Brown Butter', 'Cut Vesterhavcst'] },
 { name: 'Padrons', icon: 'i-heroicons-square-3-stack-3d', items: ['Spear Padrons', 'Garlic Oil'] },
-{ name: 'Burrata', icon: 'i-heroicons-bolt', items: [] },
 { name: 'Patatas Bravas', icon: 'i-heroicons-beaker', items: ['Do potatoes', 'Bravas Sauce', 'Ali Oli', 'Put Bravas Sauce in small bags', 'Put Ali Oli in small bags'] },
 { name: 'Roasted Pumpkin', icon: 'i-heroicons-heart', items: ['Roast Pumkin', 'Cut Cheese for Pumpkin', 'Yoghurt', 'Pumpkin Pure', 'Cryspy Kale', 'Pomergranate'] },
 { name: 'Pasta al Tartufo', icon: 'i-heroicons-archive-box', items: ['Al Tartufo Sauce', 'Mushrooms for pasta', 'Truffle Butter', 'Cut Parmesan Cheese'] },
